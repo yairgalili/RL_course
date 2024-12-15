f22109a5c6e935cda98e599f730030c8aa8f8f89
yairgalili <59600476+yairgalili@users.noreply.github.com>
2024-12-15 08:40:37 +0200

    ex3 q1

---
+++ ex3_q1.py
@@ -0,0 +1,8 @@
+"""
+Q*_M' (s, a) = Q*_M (s, a) + f(s)
+π*(s) = arg max_a Q*_M(s, a)
+
+arg max_a Q*_M'(s, a) = arg max_a Q*_M(s, a) = π*(s)
+Since s is constant w.r.t. a.q
+Therefore π* is also an optimal policy for M'.'
+"""
